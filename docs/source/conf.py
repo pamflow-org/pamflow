8282601f2200af8f3e1eebecb2f8021a57d119ac
Juan Sebastian Ulloa <jseb.ulloa@gmail.com>
2026-04-08 20:38:18 -0500

    DOC: enable myst parser in conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,8 +82,11 @@ templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-#source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
-source_suffix=[".md"]
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'myst',
+}
+# source_suffix=[".md"]
 # The master toctree document.
 master_doc = "index"
 
